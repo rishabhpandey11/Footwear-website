afc773343e64e48f1455690f749692bc4b017fda
RISHABH DINESHKUMAR PANDEY <110106009+rishabhpandey11@users.noreply.github.com>
2023-11-27 19:11:21 +0100

    Add files via upload

---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    <h1>404: Page Not Found</h1>
+</div>
+</template>
+<script>
+export default{
+    name: 'NotFoundPage',
+
+}
+</script>
+<style scoped>
+h1{
+    text-align: center;
+}
+</style>
